16e3cb504d428680bbd099f0d33c0e7b69e29def
Muntaeyeon <monty420420@naver.com>
2022-03-21 23:29:00 +0900

    router_tutorial1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './components/home';
+import Profile from './components/profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+           //index element는 기본 첫페이지
+           //element들은 기본첫페이지에서 이동할수 있는 라우터들
+    <BrowserRouter>
+           <nav>
+              <Link to="/">Home</Link> |{" "}
+              <Link to="profile">Profile</Link>
+          </nav>
+       <Routes>
+              <Route path='/' element={<App />} />
+              <Route index element={<Home />} />
+              <Route path="home" element={<Home />} />
+              <Route path="Profile" element={<Profile />} />
+       </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => (
+      <>
+          <h1>HOME</h1>
+          <button>go to Profile</button>
+      </>
+           
+    );
+
+export default Home;
