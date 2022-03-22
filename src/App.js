4955ecc75c7d398edd7095149e8e4f4efeb19d32
Muntaeyeon <monty420420@naver.com>
2022-03-22 22:48:49 +0900

    errorpage

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Home from './components/home';
 import Profile from './components/profile';
+import ErrorPage from './components/ErrorPage';
 
 function App() {
   return (
-           //index element는 기본 첫페이지
-           //element들은 기본첫페이지에서 이동할수 있는 라우터들
+           // 경로 /는 시작페이지 
+           // 경로 *은 아무경로 입력한 페이지
     <BrowserRouter>
            <nav>
               <Link to="/">Home</Link> |{" "}
               <Link to="profile">Profile</Link>
           </nav>
-       <Routes>
-              <Route path='/' element={<App />} />
-              <Route index element={<Home />} />
-              <Route path="home" element={<Home />} />
+       <Routes>         
+              <Route path="/" element={<Home />} />
               <Route path="Profile" element={<Profile />} />
+              <Route path='*' element={<ErrorPage />} />  
        </Routes>
     </BrowserRouter>
   );
